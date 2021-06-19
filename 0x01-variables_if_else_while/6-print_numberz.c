cc3b07cffaa82e6fde55f3d356f788fc00281914
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-06-18 22:00:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int numero;
+
+	for (numero = 48 ; numero <= 57 ; numero++)
+	{
+		putchar(numero);
+	}
+	putchar('\n');
+	return (0);
+}
