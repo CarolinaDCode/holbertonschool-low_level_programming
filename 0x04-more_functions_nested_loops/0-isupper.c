ae5d59aa7526950efbbdf5a0f86c17f463336047
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-06-24 21:47:08 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - review if uppercase character
+ *@c: char
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
