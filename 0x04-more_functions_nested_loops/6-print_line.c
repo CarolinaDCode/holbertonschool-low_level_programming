5791cab6a4a9efdc46bd90c626a95d2845e40b19
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-06-25 07:48:20 -0700

    6ok

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_line -  a function print that line
+ * @n: is the number of times the character _ should be printed
+ * Return: void
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+
+		for (i = 0 ; i <= n ; i++)
+		{
+			_putchar('_');
+
+		}
+	}
+	_putchar('\n');
+}
