b1ab953e0fe9b92dd0bb913646baed6e84ad6033
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-06-28 17:11:24 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,15 @@
 /**
  * _strlen - calculate the lenght of a string
  * @s: input string
- * Return: length
+ * Return: will return 1 for each element of the array that is different from null
  */
 int _strlen(char *s)
 {
-	int length;
+	int iterador = 0;
 
-	length = strlen(s);
-	return (length);
+	while (s[iterador] != '\0')
+	{
+		iterador++;
+	}
+	return (iterador);
 }
