f47cd116fe198b497189162489577b72dff2f020
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-06-30 20:51:08 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * puts_half - a function that prints half of a string
+ * @str: input string
+ * Return: Void
+ *
+ */
+void puts_half(char *str)
+{
+	unsigned int mitad, i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	if ((i + 1) % 2 == 0)
+		mitad = (i + 1) / 2;
+	else
+		mitad = i / 2;
+
+	while (mitad < i)
+	{
+		putchar(str[mitad]);
+		mitad++;
+	}
+	putchar('\n');
+}
