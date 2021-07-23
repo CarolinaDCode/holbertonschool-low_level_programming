67db8edcc34ac0b0abfc10d3c7dd44f724ef36c5
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-07-22 18:43:00 -0700

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,10 +19,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	for (; ops[i].op; i++)
+	while(ops[i].op)
 	{
 		if (*(ops[i].op) == *s && *(s + 1) == '\0')
 			return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
