786735e8cfb504eb5abcfde212a9183e2c0d9c95
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-08-06 19:44:33 -0700

    print to nth elment of linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,19 +9,18 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int cont = 0;
-    listint_t *copy_head = head;
+	unsigned int cont = 0;
+	listint_t *copy_head = head;
 
-    if (copy_head == NULL)
-        return (NULL);
+	if (copy_head == NULL)
+		return (NULL);
 
-    while (copy_head != NULL)
-    {
-        if (cont == index)
-            return (copy_head);
-        copy_head = copy_head->next;
-        cont++;
-    }
-    return (NULL);
-
+	while (copy_head != NULL)
+	{
+		if (cont == index)
+			return (copy_head);
+		copy_head = copy_head->next;
+		cont++;
+	}
+	return (NULL);
 }
