b34a2dc89873a8d7405f1cc7787d27183f66ee86
diancarhuamanta <dianacarhuamanta03@gmail.com>
2021-08-07 01:45:26 -0700

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i < idx)
 	{
+		/*hasta que i llegue a ser 5 se seguirá iterando*/
+		/**tmp_node apuntara a la siguiente estructura hasta*/
+		/*que llegue a la posicion de idx*/
 		if (tmp_node != NULL)
 			tmp_node = tmp_node->next;
 		else
@@ -28,8 +31,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	/*a new_node se le asignará el valor que se le da*/
+	/*en el main al llamar a la función*/
 	new_node->n = n;
 
+	/*En caso idx sea 0 a new_node se le asignara el valor al*/
+	/*que apunta *head que es el valor de 0*/
 	if (idx == 0)
 	{
 		new_node->next = *head;
@@ -37,6 +44,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else if (tmp_node)
 	{
+		/*En caso idx tenga un valor que no es cero a new_node que*/
+		/*anteriormente se le asigno un espacio con malloc que es una*/
+		/*estructura, esta tendra un puntero que apuntara al nodo*/
+		/*al que apunta tmp_node que es el nodo de 98, a este nodo*/
+		/*new_node asumira la posicion 5 y su puntero direccionara*/
+		/*a 98 y con tmp_nod->next = new_node, el puntero de 4 ahora*/
+		/*direccionara al nuevo puntero*/
 		new_node->next = tmp_node->next;
 		tmp_node->next = new_node;
 	}
